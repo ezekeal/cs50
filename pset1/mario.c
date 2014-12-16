5a1ca127225ae4cfa6a50bd6c0a94cddd900b1be
ezekeal <ezekeal@gmail.com>
2014-12-16 09:47:08 -0600

    Create mario.c
    added mario.c

---
+++ pset1/mario.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void)
+{
+  /* mario.c
+   * 
+   * create a mario style half pyramid of hashes based on user input
+   */
+   
+   int = height
+   //prompt user for the height (a non-negative integer no greater than 23)
+   printf("Enter a half-pyramid height [0-23]: ");
+   height = getint();
+   //re-prompt if the user enters something outside the bounds
+   
+   //generate the pyramid using printf and one or more loops
+   //be aware of alignment
+}
