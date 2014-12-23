184a04e1a0ace94cbb33d0cd85d159141d7eb04b
Zach <ezekeal@gmail.com>
2014-12-23 10:32:22 -0500

    added passing caesar.c

---
+++ pset2/caesar.c
@@ -0,0 +1,46 @@
+/**
+ * caesar.c
+ *
+ * encrypts text with a cipher
+ */
+
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+int main (int argc, char *argv[])
+{
+    if(argc != 2 || argv[1] < 0)
+    {
+        printf("Fool!\n");
+        return 1;
+    }
+    
+    int k = atoi(argv[1]) % 26;
+
+    string text = GetString();
+    
+    for(int i=0, n = strlen(text); i < n; i++)
+    {
+        char ch = text[i];
+
+        if(isalpha(ch))
+        {   
+            int mod = (int) 'a';
+
+            if isupper(ch)
+            {
+                mod = (int) 'A';    
+            }
+            ch = (char) ( (k + (int) ch - mod) % 26 + mod); 
+        }
+        
+        printf("%c", ch);
+    }
+
+    printf("\n");
+
+    return 0;
+}
