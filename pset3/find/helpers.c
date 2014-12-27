1c92bf339483e17fbcb10323406ba65ce0ac3ad6
Zach <ezekeal@gmail.com>
2014-12-27 18:49:44 -0500

    added and checked sorting algorithm

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -8,7 +8,7 @@
  */
        
 #include <cs50.h>
-
+#include <stdio.h>
 #include "helpers.h"
 
 /**
@@ -37,6 +37,21 @@ bool search(int value, int values[], int n)
  */
 void sort(int values[], int n)
 {
-    // TODO: implement an O(n^2) sorting algorithm
+    // O(n^2) sorting algorithm
+    for(int i=0; i<n; i++)
+    {
+        int smallest = values[i];
+        int smallIndex = i;
+        for (int j=i+1; j<n; j++)
+        {
+            if(values[j] < smallest)
+            {
+                smallest = values[j];
+                smallIndex = j;
+            }
+        }
+        values[smallIndex] = values[i];
+        values[i] = smallest;
+    } 
     return;
 }
